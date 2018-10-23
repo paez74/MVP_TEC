2745fc108677e2b00564a0bb08b863526dd505fe
paez74 <paez74@live.com.mx>
2018-10-23 13:02:21 -0500

    Added the global variables and changing system

---
+++ MVP/Assets/Scripts/NPC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class NPC : MonoBehaviour
+{
+   
+    
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // en caso de seleccionar el personaje rojo 
+        if (Input.GetKeyDown("1"))
+        {
+            
+                SceneManager.LoadScene(1);
+
+        } else if (Input.GetKeyDown("2")) // en caso de seleccionar el personaje verde 
+        {
+           
+                SceneManager.LoadScene(0);
+
+        }
+    }
+
+    
+
+}
