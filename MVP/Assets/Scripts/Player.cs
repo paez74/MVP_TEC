87500a2cb90f7816a1943cf5cce35e0c40e64861
paez74 <paez74@live.com.mx>
2018-11-18 21:35:17 -0600

    Added roaming NPCs ( with error on navmesh agent) and loadscene logic

---
+++ MVP/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour {
+
+    private GameObject triggeringNPC;
+    private bool triggering;
+    public GameObject npcText;
+    private int triggerScene; 
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (triggering)
+        {
+            npcText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                /*if (Input.GetKeyDown("1"))
+                {
+
+                    SceneManager.LoadScene(1);
+
+                }*/
+                print(triggerScene);
+                Destroy(triggeringNPC);
+                triggering = false; 
+            }
+
+        }
+        else
+        {
+            npcText.SetActive(false);
+        }
+
+    }
+
+    // cuando el objeto esta chocando con el NPC
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "NPC" || other.tag == "LabNPC")
+        {
+            if (other.tag == "LabNPC") triggerScene = 1; else triggerScene = 2; 
+            triggering = true;
+            triggeringNPC = other.gameObject; 
+        }
+    }
+
+    // cuando el jugador no esta chocando con el NPC
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "NPC")
+        {
+            triggering = false;
+            triggeringNPC = null; 
+        }
+    }
+
+
+}
